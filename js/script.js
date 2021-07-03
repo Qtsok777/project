722c343fb52b906c156bc263af4caa8c2dc08328
Serhiiglobal <serhii228228@gmail.com>
2021-07-03 14:28:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+ "use strict";
+
+ alert("hello");
+
+
+ const result = confirm("you f gay", );
+ console.log(result);
+
+ //const answer = prompt("are you here?", "yes");
+ 
+
+ const answers = [];
+ answers[0] = prompt(" how are you", "");
+ answers[1] = prompt("how old are you?", "");
+ answers[2] = prompt("are you hereqqwr?", "");
+
+ document.write(answers);
